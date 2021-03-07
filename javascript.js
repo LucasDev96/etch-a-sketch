21c5158505152e45fd7117c5e1abb93099f2b98e
Lucas Long <lucaslong96@gmail.com>
2021-03-07 10:02:27 -0500

    Added shadeColor() function found online to be used with shader paint style.

--- javascript.js
+++ javascript.js
@@ -19,8 +19,6 @@ slider.addEventListener("input", (e) => {
     createSketchGrid();
 });
 
-// listen for paint style buttons to be click and toggles the
-// selected class if it doesn't already have it.
 // sets new value for selectedPaintStyle and reapplies box listeners
 paintStyleButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
@@ -185,6 +183,29 @@ function generateRandomColor() {
     let color = "#" + (Math.floor(Math.random() * 8 ** 8).toString(16));
 
     return color;
+}
+
+// used to darken or lighten a color
+// taken from https://stackoverflow.com/a/13532993
+function shadeColor(color, percent) {
+
+    var R = parseInt(color.substring(1,3),16);
+    var G = parseInt(color.substring(3,5),16);
+    var B = parseInt(color.substring(5,7),16);
+
+    R = parseInt(R * (100 + percent) / 100);
+    G = parseInt(G * (100 + percent) / 100);
+    B = parseInt(B * (100 + percent) / 100);
+
+    R = (R<255)?R:255;  
+    G = (G<255)?G:255;  
+    B = (B<255)?B:255;  
+
+    var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+    var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+    var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+    return "#"+RR+GG+BB;
 }
 
 // sets the text to show the user the size of the grid
