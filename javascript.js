b76f2cdb99e42cdd08181960a7e730fd1d4e2193
Lucas Long <lucaslong96@gmail.com>
2021-03-07 11:48:59 -0500

    Got rid of the shadeColor function I added because I'm refactoring things to use rgb instead of hex colors.

--- javascript.js
+++ javascript.js
@@ -67,9 +67,11 @@ function createSketchGrid() {
 // set the background color of a box to the selectedColor
 function setBackgroundColor(box) {
     if (selectedColor === "black") {
-        box.style.backgroundColor = "black";
+        box.style.backgroundColor = "#000000";
     } else if (selectedColor === "rainbow") {
         box.style.backgroundColor = generateRandomColor();
+    } else if (selectedColor === "shader") {
+        box.style.backgroundColor = shadeColor(box.style.backgroundColor, -10);
     }
 
 }
@@ -183,29 +185,6 @@ function generateRandomColor() {
     let color = "#" + (Math.floor(Math.random() * 8 ** 8).toString(16));
 
     return color;
-}
-
-// used to darken or lighten a color
-// taken from https://stackoverflow.com/a/13532993
-function shadeColor(color, percent) {
-
-    var R = parseInt(color.substring(1,3),16);
-    var G = parseInt(color.substring(3,5),16);
-    var B = parseInt(color.substring(5,7),16);
-
-    R = parseInt(R * (100 + percent) / 100);
-    G = parseInt(G * (100 + percent) / 100);
-    B = parseInt(B * (100 + percent) / 100);
-
-    R = (R<255)?R:255;  
-    G = (G<255)?G:255;  
-    B = (B<255)?B:255;  
-
-    var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
-    var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
-    var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
-
-    return "#"+RR+GG+BB;
 }
 
 // sets the text to show the user the size of the grid
